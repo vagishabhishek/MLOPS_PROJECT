8ff5309772e8a56d948628416baa894611c6a63d
vagishabhishek <vagish.abhishek@gmail.com>
2025-11-23 15:24:25 +0530

    added project folder structure by executing template.py

---
+++ template.py
@@ -0,0 +1,56 @@
+from pathlib import Path
+
+project_name = "src"
+
+list_of_files = [
+    f"{project_name}/__init__.py",
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/components/data_ingestion.py",
+    f"{project_name}/components/data_validation.py",
+    f"{project_name}/components/data_transformation.py",
+    f"{project_name}/components/model_trainer.py",
+    f"{project_name}/components/model_evaluation.py",
+    f"{project_name}/components/model_pusher.ppy",
+    f"{project_name}/configuration/__init__.py",
+    f"{project_name}/configuration/mongo_db_connection.py",
+    f"{project_name}/configuration/aws_connction.py",
+    f"{project_name}/cloud_storage/__init__.py",
+    f"{project_name}/cloud_storage/aws_storage.py",
+    f"{project_name}/data_access/__init__.py",
+    f"{project_name}/data_access/mlops_proj_data.py",
+    f"{project_name}/constants/__init__.py",
+    f"{project_name}/entity/__init__.py",
+    f"{project_name}/entity/config_entity.py",
+    f"{project_name}/entity/artifact_entity.py",
+    f"{project_name}/entity/estimator.py",
+    f"{project_name}/entity/s3_estimator.py",
+    f"{project_name}/exception/entity.py",
+    f"{project_name}/logger/__init__.py",
+    f"{project_name}/pipeline/__init__.py",
+    f"{project_name}/pipeline/training_pipeline.py",
+    f"{project_name}/pipeline/prediction_pipeline.py",
+    f"{project_name}/utils/__init__.py",
+    f"{project_name}/utils/main_utils.py",
+    "app.py",
+    "requirements.py",
+    "Dockerfile",
+    ".dockerignore",
+    "demo.py",
+    "setup.py",
+    "pyproject.toml",
+    "config/model.yaml",
+    "config/schema.yaml",
+]
+
+for file_path in list_of_files:
+    file_path = Path(file_path)
+
+    # Create directory path
+    file_path.parent.mkdir(parents=True, exist_ok=True)
+
+    # Create file if it doesn't exist or empty it if needed
+    if not file_path.exists():
+        file_path.touch()
+        print(f"Created: {file_path}")
+    else:
+        print(f"Already exists: {file_path}")
